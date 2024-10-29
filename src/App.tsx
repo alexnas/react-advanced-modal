f542091bf263df34187968429c862b726e8771ee
Alexnander Nasokin <alexnas2python@gmail.com>
2024-10-29 18:48:18 +0300

    Initial install and clean

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-gray-200 p-3">
+        <h1 className="text-3xl font-bold text-blue-800">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
